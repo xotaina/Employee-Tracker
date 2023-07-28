7710c634ec92c3d628a7e76b52d925e4a8c9aeb4
Tanisha Barreau <tanisha.barreau101@gmail.com>
2023-07-28 11:11:53 -0400

    packages

--- connection/server.js
+++ connection/server.js
@@ -6,16 +6,12 @@ const database = sql.createConnection(
     {
         host: "localhost",
         user: "root",
-        password: "National123!",
+        password: "password",
         database: "employee_database"
     },
 
     console.log("employee_database Connected")
 );
 
-//database.connect(err => {
-    //if (err) throw err;
-    //console.log("Emplyee Tracker");
-//});
 
 module.exports = database;
